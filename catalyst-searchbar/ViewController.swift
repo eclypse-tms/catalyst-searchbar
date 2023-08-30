65b5d83c4677d8747878d3905534bd3ca5902b9e
Deniz Nessa <deniz@eclypse.io>
2023-08-30 14:11:26 -0500

    Initial Commit

---
+++ catalyst-searchbar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  
+//  Created by Turker Nessa Kucuk on 8/30/23.
+//  Copyright © 2023 Eclypse Software, LLC. All rights reserved.
+//  
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
